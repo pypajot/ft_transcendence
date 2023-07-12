0d5b4bc051ac5fa012a464a4d5385c9c0e1fbeb3
Devsstudent <devsstudent777@gmail.com>
2023-07-12 23:34:55 +0200

    saving

---
+++ frontend/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:3001/hello")
+    .then((res) => res.json())
+    .then((data) => setMessage(data.hello));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{message}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
