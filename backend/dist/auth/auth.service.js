78a5af6dae833528be63c8ad923f8881c86a3c86
ppajot <ppajot@student.42.fr>
2023-08-14 20:09:50 +0200

    hash password

---
+++ backend/dist/auth/auth.service.js
@@ -0,0 +1,39 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthService = void 0;
+const common_1 = require("@nestjs/common");
+const prisma_service_1 = require("../prisma/prisma.service");
+const argon2 = require("argon2");
+let AuthService = exports.AuthService = class AuthService {
+    constructor(prisma) {
+        this.prisma = prisma;
+    }
+    async signup(dto) {
+        console.log(dto);
+        const hash = await argon2.hash(dto.password);
+        const user = await this.prisma.user.create({
+            data: {
+                username: dto.username,
+                hash: hash,
+            },
+        });
+        return user;
+    }
+    login() {
+        return ('login');
+    }
+};
+exports.AuthService = AuthService = __decorate([
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [prisma_service_1.PrismaService])
+], AuthService);
+//# sourceMappingURL=auth.service.js.map
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -8,10 +9,11 @@ export class AuthService {
 
 	async signup(dto: AuthDto) {
 		console.log(dto);
+		const hash = await argon2.hash(dto.password);
 		const user =  await this.prisma.user.create({
 			data: {
 				username: dto.username,
-				password: dto.password,
+				hash: hash,
 			},
 		});
 		return user;
