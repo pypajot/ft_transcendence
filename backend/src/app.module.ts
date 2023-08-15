74619e0a855b835c65fde8a8155ed9ed26e5346a
ppajot <ppajot@student.42.fr>
2023-08-15 18:03:28 +0200

    import module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [ConfigModule.forRoot({
-	isGlobal: true,
-  })],
-  controllers: [AppController],
-  providers: [AppService],
-})
+	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule],
+	controllers: [AppController],
+	providers: [AppService],
+  })
 export class AppModule {}
