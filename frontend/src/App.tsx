d31678f27ff97b94a45d4c64c824bed9ec77a01a
Mael PIGNET <you@example.com>
2023-08-31 15:01:37 +0200

    adding Home folder from base_app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import io from 'socket.io-client';
 import Game from './pages/game/Game';
 import './App.css';
+import Home from './pages/Home/Home';
 
 const App: React.FC = () => {
   const socket = io("http://localhost:8000");
@@ -12,6 +13,7 @@ const App: React.FC = () => {
       <div className="game">
         <h1>Pong Game</h1>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/game" element={<Game socket={socket} />} />
         </Routes>
       </div>
