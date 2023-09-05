0763310b0fcb76bab375095c316db06998ced6ee
Mael PIGNET <you@example.com>
2023-09-05 12:23:08 +0200

    using websocket context

--- frontend/src/pages/game/modeSelection.tsx
+++ frontend/src/pages/game/modeSelection.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
-import { io } from 'socket.io-client';
+import { useState } from 'react';
+import { WebSocketContext } from '../../context/WebSocketContext';
+import { useContext } from 'react';
 
 const ModeSelection = () => {
+  const socket = useContext(WebSocketContext); // Access the WebSocket context
   const [selectedMode, setSelectedMode] = useState(null);
 
   // Function to send the selected game mode to the backend
   const sendSelectedMode = (mode: any) => {
-    socket.emit('selectGameMode', mode); // Emit the selected mode to the backend
+    socket?.emit('selectGameMode', mode); // Emit the selected mode to the backend
     setSelectedMode(mode); // Update the selected mode in the component state
   };
 
