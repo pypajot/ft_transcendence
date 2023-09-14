7ce281d4ba1c3044b8a00afb8bf7f7c36522063f
Mael PIGNET <you@example.com>
2023-09-14 18:40:15 +0200

    added Game context for 1 page logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Game from './pages/game/Game';
-import ModeSelection from './pages/game/modeSelection';
 import './App.css';
 import Home from './pages/Home/Home';
 import WebSocketProvider from './context/WebSocketContext';
+import { GameProvider } from './context/GameContext';
 
 const App: React.FC = () => {
 
@@ -13,15 +13,15 @@ const App: React.FC = () => {
       <Router>
         <div className="game">
           <h1>Pong Game</h1>
+          <GameProvider>
           <WebSocketProvider>
           <Routes>
         
             <Route path="/" element={<Home />} />
-            <Route path="/selectmode" element={<ModeSelection />} />
             <Route path="/game" element={<Game />} />
-
           </Routes>
           </WebSocketProvider>
+          </GameProvider>
         
         </div>
       </Router>
--- frontend/src/pages/game/Game.tsx
+++ frontend/src/pages/game/Game.tsx
@@ -1,135 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { useSocketContext } from '../../context/WebSocketContext.tsx';
-import {GameState} from '../../../../backend/src/game/game.service.ts';
-import './Game.css';
-import { Button, ButtonGroup, Heading } from '@twilio-paste/core';
-import { Link } from 'react-router-dom';
+import PongGame from './PongGame';
+import ModeSelection from './modeSelection';
+import { useSocketContext } from '../../context/WebSocketContext';
+import { useGameContext } from '../../context/GameContext';
+
+// Game component to render ether the game or the select mode page
 
 const Game : React.FC = () => {
-  const socket = useSocketContext(); // Access the WebSocket context
-  const [lobbyId, setLobbyId] = useState<string>(''); // The lobby ID to join
-  const [gameState, setGameState] = useState<GameState | null>(null);
-  const [countdown, setCountdown] = useState<number | null>(null);
-  const [showGo, setShowGo] = useState(false);
-  const [showBall, setShowBall] = useState(true);
-  const [gameEnd, setGameEnd] = useState(false);
-  const [gameEndMessage, setGameEndMessage] = useState('');
+	//const socket = useSocketContext(); // Access the WebSocket context
+	const {gameStart, setGameStart} = useGameContext();
 
-  useEffect(() => {
-    // Send custom event to request game state from the server
-    socket?.on('createLobby', (lobbyId: string) => {
-      setLobbyId(lobbyId);
-      socket?.emit('getGameState', { lobbyId });
-      setCountdown(3);
-      setTimeout(() => { setCountdown(2)}, 1000);
-      setTimeout(() => { setCountdown(1)}, 2000);
-      setTimeout(() => { setShowGo(true)}, 3000);
-      setTimeout(() => { socket?.emit('launchBall', { lobbyId })}, 3000);
-    },);
-
-    // Set up WebSocket event listener to receive the game state from the server
-    socket?.on('gameState', (data) => {
-      // convert the game state to a JS object
-      data = JSON.parse(data);
-      // update the game state
-      setGameState(data);
-    },);
-    //add event listener for game end
-    socket?.on('gameEnd', (data) => {
-      // display game end message
-      setGameEnd(true);
-      setShowBall(false);
-      if (data === socket?.id) {
-        setGameEndMessage('You win!');
-        setTimeout(() =>{socket?.emit('destroyLobby', { lobbyId })}, 1000);
-      }
-      else {
-        setGameEndMessage('You lose!');
-      }
-    });
-
-    return () => {
-      socket?.off('createLobby');
-      socket?.off('gameState');
-      socket?.off('gameEnd');
-    };
-  }, []);
-
-  // Other game logic and rendering based on the received gameState
-
-  const handleKeyPress = (event: any) => {
-    // Handle user input (e.g., arrow keys) for moving paddles
-    const direction = event.key === 'ArrowUp' ? 'up' : event.key === 'ArrowDown' ? 'down' : 'stop';
-    // Emit paddle movements to the server via WebSocket
-    socket?.emit('movePaddle', { direction, lobbyId});
-  };
-
-  useEffect(() => {
-    // Add event listener for user input (arrow keys)
-
-    window.addEventListener('keydown', handleKeyPress);
-    window.addEventListener('keyup', handleKeyPress);
-
-    // Clean up event listener on component unmount
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress);
-      window.removeEventListener('keyup', handleKeyPress);
-    };
-  }, [lobbyId]);
-
-  return (
-    <div className="game">
-    <div className="scores-container">
-          {/* Render scores */}
-          {gameState && (
-            <>
-              <div className="score">Player 1: {gameState.player1Score}</div>
-              <div className="score">Player 2: {gameState.player2Score}</div>
-            </>
-          )}
-        </div>
-    <div className="game-board">
-      {countdown && <div className="countdown">{countdown}</div>}
-      {showGo && <div className="go-message">GO!</div>}
-      {gameEnd && (
-        <div className="gameEnd">
-        <Heading as='h6' variant='heading20'> {gameEndMessage}</Heading>
-        <ButtonGroup>
-          <Link to="/selectmode">
-            <Button variant="secondary">Try again</Button>
-          </Link>
-          <Link to="/">
-            <Button variant="secondary">Back to home</Button>
-          </Link>
-        </ButtonGroup>
-        </div>
-      )}
-      {/* Render the ball */}
-      {gameState && showBall && (
-        <div
-          className="ball"
-          style={{
-            top: `${(gameState.ballY / gameState.gameHeight) * 100}%`,
-            left: `${(gameState.ballX / gameState.gameWidth) * 100}%`,
-          }}
-        />
-      )}
-      {/* Render paddles */}
-      {gameState && (
-        <>
-          <div
-            className="paddle paddle1"
-            style={{ top: `${(gameState.paddle1Y / gameState.gameHeight) * 100}%` }}
-          />
-          <div
-            className="paddle paddle2"
-            style={{ top: `${(gameState.paddle2Y / gameState.gameHeight) * 100}%` }}
-          />
-        </>
-      )}
-    </div>
-  </div>
-  );
+	// useEffect(() => {
+	// 	// Set up WebSocket event listener to receive the matched event from the server
+	// 	socket?.on('matched', () => {
+	// 		// redirect to game page
+	// 		setGameStart(true);
+	// 		socket?.off('matched');
+	// 	});
+	// }, []);
+	return (
+		<div>
+			{gameStart ? <PongGame /> : <ModeSelection />}
+		</div>
+	);
 };
 
 export default Game;
